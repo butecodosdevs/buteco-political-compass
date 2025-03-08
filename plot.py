6637f808b044a1b0ff7c8b336995de5bb4d8c692
Giovanni de Almeida Martins <125399281+giomartinsdev@users.noreply.github.com>
2025-03-08 20:46:50 -0300

    updating to load with csv file

--- plot.py
+++ plot.py
@@ -1,27 +1,10 @@
 from sys import stdout
 import plotly.graph_objects as go
 import random
+import pandas as pd
 
-pontos = [
-    (-0.36, -0.38, 'CWBY'), (3.75, -2.72, 'Vihugo'), (1.88, -0.15, 'Eder'),
-    (-2.5, -3.6, 'Kalango'), (-2.38, -0.77, 'Aninha'), (-0.88, -0.72, 'Sonee'),
-    (-5.25, -3.23, 'Noah'), (-0.25, -2.97, 'Funnie'), (-3.63, -4.05, 'Bruno'),
-    (4.25, -1.74, 'Miranda'), (-4.13, -3.85, 'Mango'), (5, -4.67, 'Pietro'),
-    (-4.88, -3.59, 'João'), (6.88, -2.92, 'Sebas'), (-8.5, -6.62, 'Navegantes'),
-    (0.38, -1.18, 'Cris'), (3.0, -1.59, 'Mucilon'), (6.75, -0.92, 'Gabiest'),
-    (2.0, -3.28, 'Gio'), (-4.75, -3.85, 'Wagner'), (-6.75, -5.49, 'Jamal'), 
-    (-8.63, -5.69, 'José'), (-3.13, -0.62, 'Dev Pai'), (-6.25, -3.59, 'Felurian'),
-    (-2.0, -2.67, 'Henrique'), (3.63, 1.23, 'Rina'), (-1.13, 1.95, 'Deveras'),
-    (-1.0, -0.21, 'Toninho'), (-4.38, -2.56, 'Montanha'), (0.63, 0.82, 'Math'),
-    (-3.88, -2.56, 'Log'),  (-5.5, -5.38, 'Folle'), (7.63, -2.26, '[N]amless'), 
-    (-8.63, -6.62, 'moloco'), (-7.38, -6.1, 'Professor'), (-3.75, -4.62, 'Joke'), 
-    (-7.0, -3.85, 'Bloise'),
-
-    (-4.25, 8.15, 'Hitler (Miranda)'),
-    (3.5, 1.23, 'GPT 4o, mix - Fascist'),
-    (-4.38,6.87, 'Kim Jong Un (mango)'),
-    (4.13, 2.56, 'Ianky (suspeito)') # removido por suspeita de ter feito o teste a la caralha
-]
+df_points = pd.read_csv('points.csv', sep=';')
+pontos = [tuple(x) for x in df_points.values.tolist()]
 
 colors = ['red', 'blue', 'green', 'purple', 'orange', 'magenta', 'lime', 'teal', 'navy']
 random.shuffle(colors)
@@ -103,4 +86,3 @@ fig.update_layout(
 )
 
 fig.show()
-
